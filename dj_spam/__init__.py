2ae8bf4656847cc55e089da17d53691636992f1c
Nick Kelly <kay.muneee@gmail.com>
2017-03-21 12:02:38 -0700

    chore: sanity check for settings vars

---
+++ dj_spam/__init__.py
@@ -0,0 +1,12 @@
+from django.conf import settings
+
+
+# verify user has defined necessary list in settings.
+try:
+    getattr(settings, SPAM_URLS)
+except Exception:
+    raise 'You have not defined any SPAM_URLS in your settings.'
+try:
+    getattr(settings SPAM_ROUTES)
+except:
+    raise 'You have not defined any SPAM_ROUTES in your settings.'
