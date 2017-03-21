ab9d30a3813feebb85b0b983f76acce2802e9931
Nick Kelly <kay.muneee@gmail.com>
2017-03-21 13:07:22 -0700

    feat: add colors for terminal

--- dj_spam/__init__.py
+++ dj_spam/__init__.py
@@ -1,21 +1,21 @@
 import warnings
 
 from django.conf import settings
-
+from utils import ColorMe
 
 # verify user has defined necessary list in settings.
 try:
     spam_urls = getattr(settings, 'SPAM_URLS')
 except:
-    raise AttributeError('You have not defined any SPAM_URLS in your settings.')
+    raise AttributeError(ColorMe.color_text('You have not defined any SPAM_URLS in your settings.', 'fail'))
 
 if len(spam_urls) == 0:
-    warnings.warn('You have not added any urls for spam bots to get redirected to...please add some.')
+    warnings.warn(ColorMe.color_text('You have not added any urls for spam bots to get redirected to...please add some.', 'warn'))
 
 try:
     spam_routes = getattr(settings, 'SPAM_ROUTES')
 except:
-    raise AttributeError('You have not defined any SPAM_ROUTES in your settings.')
+    raise AttributeError(ColorMe.color_text('You have not defined any SPAM_ROUTES in your settings.', 'fail'))
 
 if len(spam_routes) == 0:
-    warnings.warn('You have not added any routes for spam bots to get redirected to...please add some.')
+    warnings.warn(ColorMe.color_text('You have not added any routes for spam bots to get redirected to...please add some.', 'warn'))
