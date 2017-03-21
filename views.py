6692511f6a9a1b2631a4d80c2f9172b6807da4b9
Nick Kelly <kay.muneee@gmail.com>
2017-03-21 15:41:09 -0700

    chore: cleanup

--- views.py
+++
@@ -1,9 +0,0 @@
-import random
-
-from django.shortcuts import render, redirect
-
-from django.conf import settings
-
-
-def tickle_me_pink(request):
-    return redirect(random.choice(settings.SPAM_URLS))
