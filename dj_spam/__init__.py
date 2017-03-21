1acaa4cc99415af1c071a0a824c01a4856d723fc
Nick Kelly <kay.muneee@gmail.com>
2017-03-21 12:26:00 -0700

    chore: more error checks

--- dj_spam/__init__.py
+++ dj_spam/__init__.py
@@ -1,13 +1,21 @@
+import warnings
+
 from django.conf import settings
 
 
 # verify user has defined necessary list in settings.
 try:
-    getattr(settings, 'SPAM_URLS')
-except Exception:
+    spam_urls = getattr(settings, 'SPAM_URLS')
+except:
     raise AttributeError('You have not defined any SPAM_URLS in your settings.')
 
+if len(spam_urls) == 0:
+    warnings.warn('You have not added any urls for spam bots to get redirected to...please add some.')
+
 try:
-    getattr(settings, 'SPAM_ROUTES')
+    spam_routes = getattr(settings, 'SPAM_ROUTES')
 except:
     raise AttributeError('You have not defined any SPAM_ROUTES in your settings.')
+
+if len(spam_routes) == 0:
+    warnings.warn('You have not added any routes for spam bots to get redirected to...please add some.')
