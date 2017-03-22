b066fa43a86242b8fe76f5484f2779c448d2cf7b
Nick Kelly <kay.muneee@gmail.com>
2017-03-21 18:50:40 -0700

    feat: optional settings vars...README fix

--- dj_spam/__init__.py
+++ dj_spam/__init__.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 
 
 # common endpoints bots like
@@ -17,10 +18,20 @@ SPAM_ROUTES = [
 SPAM_URLS = [
     # donald trump saying bing bing bong
     'https://www.youtube.com/watch?v=UKbOqEk6rsk',
-    # epic sax guy
-    'https://www.youtube.com/watch?v=8ZcmTl_1ER8',
     # darth vader breathing
     'https://www.youtube.com/watch?v=un8FAjXWOBY',
     # donald trump noises
-    'https://www.youtube.com/watch?v=GQPO4nmFkCI'
+    'https://www.youtube.com/watch?v=GQPO4nmFkCI',
+    # all star windows xp remix
+    'https://www.youtube.com/watch?v=bKf9_gF5h5I',
+    # aint nobody got time for that
+    'https://www.youtube.com/watch?v=Kp9soHooisk'
 ]
+
+# check if any settings vars have been included to add to the
+# list of routes or urls
+if hasattr(settings, 'SPAM_ROUTES'):
+    SPAM_ROUTES += settings.SPAM_ROUTES
+
+if hasattr(settings, 'SPAM_URLS'):
+    SPAM_URLS += settings.SPAM_URLS
