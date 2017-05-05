02a46a31bdc9e1c70fb908b905c0fcd61d0aef4a
Nick Kelly <kay.muneee@gmail.com>
2017-05-04 18:37:02 -0700

    chore: ready app for pypi

--- setup.py
+++ setup.py
@@ -1,15 +1,43 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
 
+try:
+    from setuptools import setup, find_packages
+    from setuptools.command.test import test
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+    from setuptools.command.test import test
+import os
+
+
+here = os.path.dirname(os.path.abspath(__file__))
+f = open(os.path.join(here,  'README.rst'))
+long_description = f.read().strip()
+f.close()
 
 setup(
     name='django_spam',
-    version='1.0',
+    version='0.1.0',
     author='tivix',
     author_email='nick.kelly@tivix.com',
-    url='http://github.com/nickatnight/django-spam.git',
+    url='http://github.com/Tivix/django-spam',
     description='Redirecting bots to utilize their time better...',
-    keywords='dj spam',
+    packages=find_packages(),
+    long_description=long_description,
+    keywords='django spam',
+    zip_safe=False,
+    include_package_data=True,
     py_modules=['django_spam'],
+    install_requires=[
+        'Django>=1.8.0',
+    ],
+    classifiers=[
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development'
+    ]
 )
