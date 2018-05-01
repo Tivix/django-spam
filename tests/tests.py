95af282ab59cfac82d62695864d964293dfdd6ed
Nick Kelly <kay.muneee@gmail.com>
2018-05-01 13:08:26 -0700

    chore: update testing

--- tests/tests.py
+++ tests/tests.py
@@ -1,4 +1,3 @@
-import warnings
 import random
 
 from django.conf import settings
@@ -7,12 +6,13 @@ from django.test import TestCase, override_settings
 from django_spam import SPAM_ROUTES
 
 
-class SimpleTestCase(TestCase):
-
-    def setUp(self):
-        pass
-
+class DjangoSpamTestCase(TestCase):
     def test_redirect_random_route(self):
         response = self.client.get('/'+random.choice(SPAM_ROUTES))
         self.assertEqual(response.status_code, 302)
+    
 
+    def test_add_spam_routes_in_settings(self):
+        spam_routes = getattr(settings, 'SPAM_ROUTES', [])
+        response = self.client.get('/'+spam_routes[-1])
+        self.assertEqual(response.status_code, 302)
